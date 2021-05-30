217a2b88a2e51ff9c5f938112b9313dbbfade818
Juan Rios <mrdaniel19996@gmail.com>
2021-05-30 14:16:13 -0500

    working~!

---
+++ pages/api/data.js
@@ -0,0 +1,8 @@
+export const MESSAGES = {
+  default: "Intenta una nueva clave",
+  test: "test1",
+  test2: "test2",
+  meencantas: "alee",
+};
+
+export const DEFAULT_MESSAGE = "Encontraras la clave para un mensaje secreto?";
